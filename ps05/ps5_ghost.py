43839d4c7957fa5ef4e452fafdc97c97560edd94
Pat Dayton <daytonpe14@gmail.com>
2017-12-04 10:14:11 -0600

    PS5 Complete

--- ps05/ps5_ghost.py
+++ ps05/ps5_ghost.py
@@ -20,14 +20,14 @@ def load_words():
     Depending on the size of the word list, this function may
     take a while to finish.
     """
-    print "Loading word list from file..."
+    print('Loading word list from file...')
     # inFile: file
-    inFile = open(WORDLIST_FILENAME, 'r', 0)
+    inFile = open(WORDLIST_FILENAME, 'r', encoding='ascii')
     # wordlist: list of strings
     wordlist = []
     for line in inFile:
         wordlist.append(line.strip().lower())
-    print "  ", len(wordlist), "words loaded."
+    print("  ", len(wordlist), "words loaded.")
     return wordlist
 
 def get_frequency_dict(sequence):
@@ -55,3 +55,76 @@ def get_frequency_dict(sequence):
 wordlist = load_words()
 
 # TO DO: your code begins here!
+def word_ends_game(word, word_list):
+    if len(word)<4:
+        return False
+    else:
+        if word.lower() in word_list:
+            return True
+        else: 
+            return False
+
+def word_can_be_formed(word, word_list):
+    for entry in word_list:
+        if word.lower() == entry[0:len(word)]:
+            return True
+    return False
+
+
+
+def play_game(word_list):
+
+    print()
+    print()
+
+    turn = 1;
+
+    word = ''
+
+    while (True):
+        if turn%2==1:
+            letter = input("Player 1: ")
+            
+        else:
+            letter = input("Player 2: ")
+
+        if letter not in string.ascii_letters:
+            print("That's not a letter. Please Try again.")
+            continue
+
+
+        turn = turn + 1
+        word = word+letter
+
+        if word_can_be_formed(word, word_list)==False:
+            print('GAME OVER! ' +str(word.upper())+' does not start a word')
+            word = ''
+            return 0
+
+        print()
+        print(word.upper())
+
+        if(word_ends_game(word, word_list)):
+            print('GAME OVER! ' +str(word.upper())+' is a word.')
+            word = ''
+            return 0
+
+
+def play_ghost(word_list):
+    print()
+    print('GHOST')
+    while True:
+        cmd = input('n: new game\ne: end game\n')
+        if cmd == 'n' or cmd == 'N':
+            play_game(word_list)
+            print()
+        elif cmd == 'e' or cmd == 'E':
+            break
+        else:
+            print("Invalid command.") 
+
+
+play_ghost(wordlist)
+
+
+
